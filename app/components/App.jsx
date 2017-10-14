b8fdb5958b60dc9ec92a622e05577c981c091ec0
Charly Palencia <charly@fullstacklabs.co>
2017-10-13 19:30:03 -0500

    First Component w/o bind

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,10 +2,26 @@ import React from 'react';
 import LoginPage from './LoginPage';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      alert: {
+        show: false,
+        message: ''
+      }
+    };
+  }
+
+  handleShowAlert(message){
+    console.log(message);
+    this.setState({alert: { show: true, message }});
+  }
+
   render() {
+    const { alert } = this.state;
     return (
       <div id="content">
-        <LoginPage />
+        <LoginPage alert={alert} onShowAlert={(message, e) => this.handleShowAlert(message, e)}/>
       </div>
     );
   }
--- app/components/LoginPage.js
+++ app/components/LoginPage.js
@@ -1,31 +1,53 @@
 import React from 'react';
 
 export default class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  handleLogin(event){
+    event.preventDefault();
+    const { email, password } = this.state;
+    if(!email && !password){
+      this.props.onShowAlert('i need data!');
+    }
+  }
+
   render() {
+    const { alert } = this.props;
+    const { email, password } = this.state;
+
     return (
       <form>
         <div>
+          <label htmlFor="name">
+            User
+          </label>
           <input
             type="user"
             id="email"
             name="email"
-            value=""
+            value={email}
           />
-          <label htmlFor="name">
-            User
-          </label>
         </div>
         <div>
+          <label htmlFor="name">
+            Password
+          </label>
           <input
             type="password"
             id="password"
             name="password"
-            value=""
+            value={password}
           />
-          <label htmlFor="name">
-            Password
-          </label>
         </div>
+      <button onClick={(e) => this.handleLogin(e)}>
+          Login
+        </button>
       </form>
     )
   }
